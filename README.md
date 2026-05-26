65bfd7b6c32091aed45a0fdaa0988caa2a488c47
Nate Halverson <nathandhalverson@gmail.com>
2026-05-26 13:55:49 -0500

    front end js -> ts

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ This is meant to be a true production level build out
 
 ## Codebase
 Dunnit is a monorepo appliation that includes 
-* the frontend (/fe-dunnit): in react app surved with vite
+* the frontend (/fe-dunnit): in react typescript app surved with vite 
 * the backend (/be-dunnit): a in dotnet web api sitting ontop of a sql database
 
--- fe-dunnit/src/main.tsx
+++ fe-dunnit/src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.tsx'
+
 
 const config = await fetch('http://localhost:5235/config').then((r) => r.json())
 console.log('testValue:', config.testValue)
 
-createRoot(document.getElementById('root')).render(
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
